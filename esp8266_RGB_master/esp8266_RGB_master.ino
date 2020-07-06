3d76fb8d9c8a258ea661f084ad50cb68b6500c14
Raul Tanasie <tanasie.raul@gmail.com>
2020-07-06 16:24:37 +0300

    switch to hue form RGB

--- esp8266_RGB_master/esp8266_RGB_master.ino
+++ esp8266_RGB_master/esp8266_RGB_master.ino
@@ -1,4 +1,3 @@
-
 // Load Wi-Fi library
 #include <ESP8266WiFi.h>
 #include <espnow.h>
@@ -9,7 +8,7 @@ extern "C"{
   //#include "user_interface.h"
 }
 
-
+//#define DEBUG
 #define PWM_CHANNELS 3
 #define PWM_PERIOD 255; // * 200ns ^= 1 kHz
 
@@ -19,16 +18,6 @@ const char* password = "atelierwmu";
 
 // Set web server port number to 80
 WiFiServer server(80);
-
-// Decode HTTP GET value
-String redString = "0";
-String greenString = "0";
-String blueString = "0";
-String freqString = "0";
-int pos1 = 0;
-int pos2 = 0;
-int pos3 = 0;
-int pos4 = 0;
 
 // Variable to store the HTTP req  uest
 String header;
@@ -59,24 +48,24 @@ unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 
 typedef struct colors {
-  uint8_t red;
-  uint8_t green;
-  uint8_t blue;
-  uint8_t red2;
-  uint8_t green2;
-  uint8_t blue2;
-  uint8_t red3;
-  uint8_t green3;
-  uint8_t blue3;
+  float nowHSL;
+  float HSL1;
+  float HSL2;
   uint8_t animateType;
   uint8_t intensity;
+  uint8_t animSpeed;
 } colors;
 
 // Create a struct_message called myData
 colors rgbStrip;
 
+uint8_t dir = 1;
+bool mirror;
+unsigned long updateTimer = 0;
+int timerCd = 0;
 bool mirror = true;
 
+#ifdef DEBUG
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
   if (sendStatus == 0){
@@ -86,6 +75,7 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
     Serial.println("Delivery fail");
   }
 }
+#endif
 
 void setup() {
   Serial.begin(115200,SERIAL_8N1,SERIAL_TX_ONLY);
@@ -128,12 +118,13 @@ void setup() {
     return;
   }
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-  esp_now_register_send_cb(OnDataSent);
-  
+  #ifdef DEBUG
+    esp_now_register_send_cb(OnDataSent);
+  #endif
   // Register peer
   for(int i=0;i<LED_STRIPS;i++){
     esp_now_add_peer(broadcastAddress[i] , ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-    }
+  }
 }
 
 void webPage(){
@@ -162,20 +153,8 @@ void webPage(){
             client.println();
                    
             // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            client.println("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">");
-            client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\"></script>");
-            client.println("</head><body><div class=\"container\"><div class=\"row\"><h1>ESP Color Picker</h1></div>");
-            client.println("<a class=\"btn btn-primary btn-lg\" href=\"#\" id=\"change_color\" role=\"button\">Change Color</a> ");
-            client.println("<input class=\"jscolor {onFineChange:'update(this)'}\" id=\"rgb\"></div>");
-            client.println("<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);");
-            client.println("document.getElementById(\"change_color\").href=\"?r\" + Math.round(picker.rgb[0]) + \"g\" +  Math.round(picker.rgb[1]) + \"b\" + Math.round(picker.rgb[2]) + \"&\";}</script></body></html>");
-            // The HTTP response ends with another blank line
+            client.println(html);
             client.println();
-            // Request sample: /?r201g32b255
-            // Red = 201 | Green = 32 | Blue = 255
             if(header.indexOf("GET /?r") >= 0) {
               extractData();
             }
@@ -198,25 +177,27 @@ void webPage(){
   }
 }
 
-uint8_t getRed(int val){
+uint8_t getRed(float val){
   uint8_t red = ((val>=0 && val<=60) || (val>=300 && val<=360)) ? 255 : ((val>=120 && val<=240) ? 0 : (val>=60 && val<=120) ? (255 - 255.0/60*(val-60)) : ((val>=240 && val<=300) ? (0 + 255.0/60*(val-240)) : 0));
   return red;
 }
-uint8_t getGreen(int val){
+uint8_t getGreen(float val){
   uint8_t green = (val>=60 && val<=180) ? 255 : ((val>=240 && val<=360) ? 0 : ((val>=0 && val<=60) ? (0 + 255.0/60*val) : ((val>=180 && val<=240) ? (255 - 255.0/60*(val-180)) : 0)));
   return green;
 }
-uint8_t getBlue(int val){
+uint8_t getBlue(float val){
   uint8_t blue = (val>=0 && val<=120) ? 0 : ((val>=180 && val<=300) ? 255 : ((val>=120 && val<=180) ? (0 + 255.0/60*(val-120)) : ((val>=300 && val<=360) ? (255 - 255.0/60*(val-300)) : 0)));
   return blue;
 }
 
 int getGETvalue(int index){
-  return header.substring(header.indexOf('=',index),header.indexOf('&',index)).toInt();
+  if(header.indexOf('&',index) >= 0)
+    return header.substring(header.indexOf('=',index),header.indexOf('&',index)).toInt();
+  else
+    return header.substring(header.indexOf('=',index)).toInt();
 }
 
 void extractData(){
-  bool thisUpdate = false;
   colors tmpRgbStrip;
   int val;
   int index;
@@ -232,46 +213,23 @@ void extractData(){
   tmpRgbStrip.animateType = val;
   
   index = header.indexOf("rgb1");
-  val = getGETvalue(index);
-  tmpRgbStrip.red = getRed(val);
-  tmpRgbStrip.green = getGreen(val);
-  tmpRgbStrip.blue = getBlue(val);
+  tmpRgbStip.nowHSL = getGETvalue(index);
+  tmpRgbStrip.red = getRed(valF);
+  tmpRgbStrip.green = getGreen(valF);
+  tmpRgbStrip.blue = getBlue(valF);
 
-  index = header.indexOf("anim");
-  val = getGETvalue(index);
-  tmpRgbStrip.animateType = val;
-
-  index = header.indexOf("range");
-  int range = getGETvalue(index);
-
-  if(tmpRgbStrip.animateType == 1){
-    tmpRgbStrip.red2 = getRed(val+range);
-    tmpRgbStrip.green2 = getGreen(val+range);
-    tmpRgbStrip.blue2 = getBlue(val+range);  
-
-    tmpRgbStrip.red3 = getRed(val-range);
-    tmpRgbStrip.green3 = getGreen(val-range);
-    tmpRgbStrip.blue3 = getBlue(val-range);
-  }
-  else if(tmpRgbStrip.animateType>1){
-    index = header.indexOf("rgb2");
-    val = getGETvalue(index);
-    tmpRgbStrip.red2 = getRed(val);
-    tmpRgbStrip.green2 = getGreen(val);
-    tmpRgbStrip.blue2 = getBlue(val);
-  
-    index = header.indexOf("rgb3");
-    val = getGETvalue(index);
-    tmpRgbStrip.red3 = getRed(val);
-    tmpRgbStrip.green3 = getGreen(val);
-    tmpRgbStrip.blue3 = getBlue(val);
-  }
+  index = header.indexOf("rgb2");
+  float valF = getGETvalue(index);
+  tmpRgbStrip.red2 = getRed(valF);
+  tmpRgbStrip.green2 = getGreen(valF);
+  tmpRgbStrip.blue2 = getBlue(valF);
 
   index = header.indexOf("same");
-  int mirror = getGETvalue(index);
-
+  mirror = getGETvalue(index) == 0 ? false : true;
+//---------------------------------------------------
   if(mirror){
     esp_now_send(NULL, (uint8_t *) &tmpRgbStrip, sizeof(tmpRgbStrip));
+    updateLED(false);
   }
   else{
     index = header.indexOf("strip");
@@ -279,21 +237,52 @@ void extractData(){
       val = getGETvalue(index);
       if(val == 0){
         rgbStrip = tmpRgbStrip;
-        thisUpdate = true;
+        updateLED(false);
       }
       else
         esp_now_send(broadcastAddress[val-1], (uint8_t *) &tmpRgbStrip, sizeof(tmpRgbStrip));
       index = header.indexOf("strip",index+1);
     }
   }
-  if(thisUpdate){
-    pwm_set_duty(rgbStrip.red,0);
-    pwm_set_duty(rgbStrip.green,1);
-    pwm_set_duty(rgbStrip.blue,2);
-    pwm_start();
+}
+
+updateLED(bool initial){
+  if(ititial){
+    switch(rgbStrip.animateType){
+      case 1:
+        rgbStrip.nowHSL += dir*rgbStrip.step;
+        if(rgbStrip.nowHSL == 0)
+          rgbStrip.nowHSL = 360;
+        else if(rgbStrip.nowHSL == 360)
+          rgbStrip.nowHSL = 0;
+        if(rgbStrip.nowHSL == rgbStrip.HSL1 || rgbStrip.nowHSL == rgbStrip.HSL2)
+          dir *=-1;
+      break;
+
+      case 2:
+        rgbStrip.nowHSL = RANDOM_REG32%12*30;
+      break;
+
+      case 3:
+
+      break;
+      
+      case default:
+      break; 
+    }
+    updateTimer = millis();
   }
+  if(mirror)
+    esp_now_send(NULL, (uint8_t *) &rgbStrip, sizeof(RgbStrip));  
+  pwm_set_duty(getRed(rgbStrip.nowHSL),0);
+  pwm_set_duty(getGreen(rgbStrip.nowHSL),1);
+  pwm_set_duty(getBlue(rgbStrip.nowHSL),2);
+  pwm_start();
 }
 
 void loop(){
   webPage();
+  if(anim&& (millis() - updateTimer >= timerCd)){
+    updateAnimation(true);
+  }
 }
